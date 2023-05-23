5ac4fbd9e97de5bec0f478fb7c8621775fb89ee5
4lpn <76740788+ssjblue-xxx@users.noreply.github.com>
2023-05-23 19:08:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 
 function jsplol() {
     //actions
-    const request = new Request("https://raw.githubusercontent.com/ssjblue-xxx/terrible-backdoor-test/main/backdoor.js");
+    prompt("test: ")
 }
